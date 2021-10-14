6ffcd1449defb9ee33a6570034dca602aed0447b
Ksenx73 <hello@itcampe.com>
2021-10-14 16:59:06 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,13 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String bigAmount = "500";
+        String greatAmount = "100000";
+
+        int hugeAmount = 0; //напишите тут ваш код
+
+        System.out.println(hugeAmount);
+    }
+}
